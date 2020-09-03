8bd3e4c3aed5478a98d2e1f2dd60cf5e670b9c9b
JosueXA <josue.xicotencal.a@gmail.com>
2020-09-02 19:28:14 -0500

    add stripe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
+import { Stripe } from '@ionic-native/stripe/ngx';
+
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -16,6 +18,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    Stripe,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Stripe } from '@ionic-native/stripe/ngx';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,22 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private stripe: Stripe) {
+    console.log('caca home');
+    
+    this.stripe.setPublishableKey('pk_test_ZOS306v6VUm4b4FPpfsDcuvE00Gv7W5YXp');
+    
+    let card = {
+    number: '4242424242424242',
+    expMonth: 12,
+    expYear: 2020,
+    cvc: '220'
+    }
+  
+    this.stripe.createCardToken(card)
+      .then(token => console.log(token.id))
+      .catch(error => console.error(error));
+  }
+  
 
 }
